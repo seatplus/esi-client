602ba9763ae65f22c1371f2b6f3e1078b511e520
Herpaderp Aldent <felix.a.huber@gmx.net>
2024-09-18 20:09:25 +0200

    Refactored UpdateRefreshTokenService constructor for dependency injection.

--- src/Services/UpdateRefreshTokenService.php
+++ src/Services/UpdateRefreshTokenService.php
@@ -2,9 +2,6 @@
 
 namespace Seatplus\EsiClient\Services;
 
-use Firebase\JWT\ExpiredException;
-use Firebase\JWT\JWK;
-use Firebase\JWT\JWT;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ServerException;
@@ -12,15 +9,19 @@ use GuzzleHttp\RequestOptions;
 use Seatplus\EsiClient\DataTransferObjects\EsiAuthentication;
 use Seatplus\EsiClient\DataTransferObjects\EsiResponse;
 use Seatplus\EsiClient\Exceptions\RequestFailedException;
-use UnexpectedValueException;
 
 class UpdateRefreshTokenService
 {
-    /**
-     * Tranquility endpoint for retrieving user info.
-     */
-    public const TRANQUILITY_ENDPOINT = 'https://login.eveonline.com';
-    private Client $client;
+    CONST TOKEN_URL = 'https://login.eveonline.com/v2/oauth/token';
+
+    public function __construct(
+        private ?Client $client = null,
+        private ?VerifyAccessToken $verifyAccessToken = null
+    )
+    {
+        $this->client = $client ?? new Client();
+        $this->verifyAccessToken = $verifyAccessToken ?? new VerifyAccessToken();
+    }
 
     /**
      * @throws RequestFailedException
@@ -31,7 +32,7 @@ class UpdateRefreshTokenService
         $authorization = 'Basic '.base64_encode($authentication->client_id.':'.$authentication->secret);
 
         try {
-            $response = $this->getClient()->post($this->getTokenUrl(), [
+            $response = $this->client->post(self::TOKEN_URL, [
                 RequestOptions::HEADERS => [
                     'Authorization' => $authorization,
                 ],
@@ -56,52 +57,8 @@ class UpdateRefreshTokenService
         // Values are access_token // expires_in // token_type // refresh_token
         $payload = json_decode((string) $response->getBody(), true);
 
-        $this->verify($payload['access_token']);
+        $this->verifyAccessToken->verify($payload['access_token']);
 
         return $payload;
-    }
-
-    protected function getTokenUrl()
-    {
-        return 'https://login.eveonline.com/v2/oauth/token';
-    }
-
-    private function verify($jwt)
-    {
-        $responseJwks = $this->client->get('https://login.eveonline.com/oauth/jwks');
-        $responseJwksInfo = json_decode((string) $responseJwks->getBody(), true);
-        $decodedArray = (array) JWT::decode($jwt, JWK::parseKeySet($responseJwksInfo), ['RS256']);
-
-        if ($decodedArray['iss'] !== 'login.eveonline.com' && $decodedArray['iss'] !== self::TRANQUILITY_ENDPOINT) {
-            throw new UnexpectedValueException('Access token issuer mismatch');
-        }
-
-        if (time() >= $decodedArray['exp']) {
-            throw new ExpiredException();
-        }
-
-        return $decodedArray;
-    }
-
-    /**
-     * @return Client
-     */
-    public function getClient(): Client
-    {
-        if (! isset($this->client)) {
-            $this->setClient(new Client());
-        }
-
-        return $this->client;
-    }
-
-    /**
-     * @param Client $client
-     */
-    public function setClient(Client $client): self
-    {
-        $this->client = $client;
-
-        return $this;
     }
 }
